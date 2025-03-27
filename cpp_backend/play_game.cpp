727cbb8dd3c94dd8cfdff4eaa24aef874420753c
Wayne Chen <wchen206@my.bcit.ca>
2025-03-27 15:09:19 -0700

    cleaned up files and code

--- cpp_backend/play_game.cpp
+++ cpp_backend/play_game.cpp
@@ -31,7 +31,7 @@ int main(int argc, char* argv[]) {
     //       "random" for random moves for both sides,
     //       "ai_vs_random" for AI vs random (Black uses AI, White random)
     int winningThreshold = 3;
-    int aiDepth = 5;
+    int aiDepth = 3;
     int timeLimitMs = 3000;
     std::string mode = "ai_vs_random"; // Options: "ai", "random", or "ai_vs_random"
 
