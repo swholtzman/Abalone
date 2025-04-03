5089d18a2d1917931090ab531bcfb89aca037cbc
Ila Golbaghi-Koukia <145630405+ilagko@users.noreply.github.com>
2025-04-02 17:01:21 -0700

    Added windows build tools

---
+++ install_dependencies.ps1
@@ -0,0 +1,16 @@
+# install_dependencies.ps1
+# Run as Administrator.
+# Install Chocolatey (if not already installed)
+# If you want to remove it: Remove-Item C:\ProgramData\chocolatey -Recurse -Force
+
+Set-ExecutionPolicy Bypass -Scope Process -Force
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install Python 3.13
+choco install python -y
+
+# Install CMake
+choco install cmake -y
+
+# Install MinGW (includes g++)
+choco install mingw -y
